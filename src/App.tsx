1384b7a69e5631516d932e232ed122d7e27c2220
Yash Masani <yash.masani@gmail.com>
2022-11-05 15:29:59 -0400

    added beautiful randomess and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import Notes from './Notes';
@@ -7,6 +7,11 @@ import "./App.css";
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const SPREAD = 10;
+
+  function colorSpawner(): number {
+    return Math.ceil(Math.random() * 255);
+  }
 
   async function greet() {
     setGreetMsg(await invoke("greet", { name }));
@@ -14,7 +19,13 @@ function App() {
 
   return (
     <div className="container">
-      <span className="container-ball"></span>
+      <span className="container-ball">
+        {
+          [...Array(6)].map((_, i) => (
+            <span style={{ backgroundColor: `rgb(${colorSpawner()}, ${colorSpawner()}, ${colorSpawner()})`, left: `${30 + (i*SPREAD)}%`, animationDelay: `${colorSpawner() - (5 * SPREAD)}s` }} key={i} />
+          ))
+        }
+      </span>
       <h1 className="container-title"><u>Welcome to My Notes!</u></h1>
       <Notes/>
     </div>
